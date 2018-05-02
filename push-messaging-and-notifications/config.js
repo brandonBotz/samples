68147633f89a3e512551b0341a8a7d8e3e5101a6
Brandon Botzet <1984903+brandonBotz@users.noreply.github.com>
2018-05-02 12:54:25 -0500

    Update and rename config.sample.js to config.js

---
+++ push-messaging-and-notifications/config.js
@@ -0,0 +1,4 @@
+window.GoogleSamples = window.GoogleSamples || {};
+window.GoogleSamples.Config = window.GoogleSamples.Config || {
+  gcmAPIKey: 'AAAAHORr9iw:APA91bHN5IbUrmqWz7T0OeH0jvHO0YpMmoodijujiqUPm_UV5ot1ESDBGs6Ab1UpnS3v66fUwUopI6Sz-5c0JAcPJYGT_lhr0pV9wHzxWWXsGjosEOstYbxt8fuvX9cvQj015h1NOfap'
+};
